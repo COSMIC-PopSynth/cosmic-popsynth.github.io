f1a27c3b7c34cfffac13c0304c821ae08927db24
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-04-30 13:41:56 -0500

    It has been a little while since we updated these docs online at all. This gives the user an actual idea of some of what this can do

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0+untagged.257.g57c92fb.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
