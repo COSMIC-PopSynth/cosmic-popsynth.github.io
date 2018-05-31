9ba7c41e48f9f79aba99d496b47b495ee98a23ca
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-05-31 14:32:33 -0500

    Doing great things

--- _static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0+untagged.259.g810a3e7.dirty',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
