c1c6cca50b65da2746c4a35bf191ac6542d321c7
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-06-01 09:48:09 -0500

    updating docs

--- _static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0+untagged.299.gb0e8cc6',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
