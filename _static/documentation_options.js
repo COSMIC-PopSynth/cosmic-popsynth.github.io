26a9272c68a762ddac6990342d5292a91b636353
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-08-14 14:09:46 -0500

    Some more fine looking docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.299.gb0e8cc6',
+    VERSION: '0+untagged.334.g4baa772.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
