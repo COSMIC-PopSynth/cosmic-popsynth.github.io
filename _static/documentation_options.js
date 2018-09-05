3f5fe5e97a6d5f42422bd5f2b720727d7fe5a9aa
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-09-05 12:51:17 -0400

    doing a big doc clean up

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.379.gc27d8f6',
+    VERSION: '0+untagged.390.g8dafa79.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
