edef1dfa7961422d2179c53d169db966274c55f8
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2019-02-07 21:17:59 -0500

    updating fixed pop docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2+26.g35d5ddf.dirty',
+    VERSION: '1.0.2+109.gd8e36c3.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
