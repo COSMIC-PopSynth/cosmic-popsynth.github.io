5a3c8d2c1ff6a3bbb73a3d59bc4c2411dde55dab
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2019-02-07 22:49:43 -0500

    final update for the night

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2+109.gd8e36c3.dirty',
+    VERSION: '1.0.2+119.g78d15b7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
