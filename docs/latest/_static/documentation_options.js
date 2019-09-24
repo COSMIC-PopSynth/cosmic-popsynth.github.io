ec4aa16c4d97fdc9e48e2f559a3aab34879300ce
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2019-09-24 10:49:11 -0500

    New development docs

--- docs/latest/_static/documentation_options.js
+++ docs/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0-beta+6.g810e084.dirty',
+    VERSION: '3.0.0-beta+55.g5b03dbc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
